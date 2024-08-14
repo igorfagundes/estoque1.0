e4cc46fecccadd63dfd30a44f5d04151dcc63a24
igor fagundes pontes <128741930+igorfagundes@users.noreply.github.com>
2024-08-14 14:05:30 -0300

    linkando
    linkando jpa com mysql

---
+++ src/main/java/com/thecode/controledeestoque/ControledeestoqueApplication.java
@@ -0,0 +1,13 @@
+package com.thecode.controledeestoque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControledeestoqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControledeestoqueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thecode/controledeestoque/ControledeestoqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thecode.controledeestoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControledeestoqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
