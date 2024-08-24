0b4292fca9aa7c1f4dc3b6ec3e296d1ba43050c9
igor fagundes pontes <128741930+igorfagundes@users.noreply.github.com>
2024-08-24 16:46:58 -0300

    resolvi começar do zero
    tinha muitos bugs e erros, fiquei 4 dias pra configurar um metodo de cadastro

--- src/main/java/com/thecode/controledeestoque/produtorepository/ProdutoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.thecode.controledeestoque.produtorepository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.thecode.controledeestoque.model.Produto;
-
-public interface ProdutoRepository extends JpaRepository<Produto, Long> {
-    Produto findBynome(String nome);
-}
